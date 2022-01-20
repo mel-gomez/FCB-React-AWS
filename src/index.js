0373f3ccc9139cbc321fecb67d31a5b431ddb19c
romfi <romel_gomez11@yahoo.com>
2022-01-20 16:44:47 +0800

    Create a new TasksList component

--- src/index.js
+++ src/index.js
@@ -12,13 +12,18 @@ function App() {
             <header className="app-header">
                 <h1>My Amazing ToDo-List App</h1>
                 <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
-                <ul>
-                    <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
-                    <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
-                    <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
-                </ul>
+                <TasksList />
             </header>
         </div>
+    );
+}
+function TasksList() {
+    return (
+        <ul>
+            <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+            <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+            <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+        </ul>
     );
 }
 
